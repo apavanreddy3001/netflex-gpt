3d9262421790c9cf8286b346fa84c01cf9a497f1
Pawanreddy <apavanreddy1003@gmail.com>
2025-03-19 00:08:59 +0530

    adding the inital setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className=' text-3xl font-bold text-green-800 ' >welcome </div>        
+    </>
+  )
+}
+
+export default App
